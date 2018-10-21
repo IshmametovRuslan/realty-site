8ea7275cf0001d78106ae442c9b7fa2d665a64a3
Ruslan <russ-i@yandex.ru>
2018-10-21 15:06:29 +0300

    added picture themes and the connection of the stylesheets of the parent and child themes

--- functions.php
+++ functions.php
@@ -5,3 +5,13 @@
  * Date: 21.10.2018
  * Time: 14:38
  */
+
+
+/*Функция подключения стилей родительской и дочерней темы*/
+
+add_action( 'wp_enqueue_scripts', 'my_theme_styles' );
+function my_theme_styles() {
+	wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/css/theme.css' );
+
+	wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() .'/style.css', array('parent-theme-css') );
+}
